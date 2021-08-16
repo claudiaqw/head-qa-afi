53f0cdd66b17bae5dbc4ffc79d5aced0a2d9f143
Claudia Quintana Wong <claudiaqw94@gmail.com>
2021-08-16 23:04:08 +0200

    datasets saved

--- code/training.py
+++ code/training.py
@@ -27,6 +27,32 @@ def train(model, optimizer, train_dl, test_dl, validate, epochs=50):
               (i, train_loss, valid_loss, valid_acc))
     return epochs_results
 
+
+def train_ir(model, optimizer, train_dl, test_dl, validate, epochs=50):
+    y_trues, y_preds = [], []
+    epochs_results = []
+    for i in range(epochs):
+        model.train()
+        total = 0
+        sum_loss = 0
+        for x_0, x_1, y in train_dl:
+            batch = y.shape[0]
+            out = model(x_0.long(), x_1.long())
+            loss = F.binary_cross_entropy(out, y.float())
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            total += batch
+            sum_loss += batch*(loss.item())
+        train_loss = sum_loss/total
+        valid_loss, valid_acc, y_real, y_pred = validate(model, test_dl)
+        y_trues.append(y_real)
+        y_preds.append(y_pred)
+        epochs_results.append([train_loss, valid_loss, valid_acc])
+        print("Epoch %s train loss  %.4f valid loss %.3f and accuracy %.4f" %
+              (i, train_loss, valid_loss, valid_acc))
+    return epochs_results
+
 def validate(model, dataloader):
     model.eval()
     loss, right, total = 0, 0, 0
@@ -34,6 +60,22 @@ def validate(model, dataloader):
     for x, y in dataloader:
         batch = y.shape[0]
         out = model(x.long())
+        loss = F.binary_cross_entropy(out, y.float())
+        loss += batch*(loss.item())
+        total += batch
+        pred = torch.max(out, dim=1)[1]
+        y_true.append(y)
+        y_preds.append(pred)
+        right += (pred == y).float().sum().item()
+    return loss/total, right/total, y_true, y_preds
+
+def validate_ir(model, dataloader):
+    model.eval()
+    loss, right, total = 0, 0, 0
+    y_true, y_preds = [], []
+    for x_0, x_1, y in dataloader:
+        batch = y.shape[0]
+        out = model(x_0.long(), x_1.long())
         loss = F.binary_cross_entropy(out, y.float())
         loss += batch*(loss.item())
         total += batch
