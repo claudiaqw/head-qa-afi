c2c543d63f6b1bb7178da55985843c7943e4dc46
Claudia Quintana Wong <cquintana@imantia.com>
2021-08-24 18:09:57 +0200

    bert

--- code/training.py
+++ code/training.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn.functional as F
+from transformers import BertTokenizer
 
 import numpy as np
 
@@ -145,5 +146,124 @@ def make_embedding_matrix(filepath, words, word_to_idx=None, glove_embeddings=No
             final_embeddings[i,:] = embedding_i
     return final_embeddings
 
+# BERT tools
+def pad_seq(x, seq_len=110, right_padding = False):
+    z = np.zeros(seq_len, dtype=np.int32)
+    n = min(seq_len, len(x))
+    if right_padding:
+        z[:n] = x[0:n]
+    else:
+        z[(seq_len - n):] = x[0:n]
+    return z
 
+def encode_bert_ir(samples, tokenizer):
+    input_ids_0, input_ids_1, labels = [], [], []
+    for item in samples:
+        encoded_q = tokenizer.encode(item['question'], add_special_tokens = True)
+        encoded_a = tokenizer.encode(item['answer'], add_special_tokens = True)
+        padded_q = pad_seq(encoded_q, seq_len=30)
+        padded_a = pad_seq(encoded_a, seq_len=30)
+        input_ids_0.append(padded_q)
+        input_ids_1.append(padded_a)
+        labels.append(item['label'])
+        
+    attention_masks_0, attention_masks_1 = [], []
+    for sent in input_ids_0:  
+        att_mask = [int(token_id > 0) for token_id in sent]
+        attention_masks_0.append(att_mask)
+    
+    for sent in input_ids_1:  
+        att_mask = [int(token_id > 0) for token_id in sent]
+        attention_masks_1.append(att_mask)
+        
+    return input_ids_0, attention_masks_0, input_ids_1, attention_masks_1, labels
 
+def encoder_bert_ir_instance(sample, tokenizer=BertTokenizer.from_pretrained('dccuchile/bert-base-spanish-wwm-cased', do_lower_case=False)):    
+    qtext, answers = sample['qtext'], sample['answers']    
+    encoded_q = tokenizer.encode(qtext, add_special_tokens = True)
+    padded_q = pad_seq(encoded_q, seq_len=30)
+    right_answer = sample['ra']
+    
+    input_ids_0, input_ids_1, labels = [], [], []
+    right_answer = sample['ra']
+    for answer in answers:
+        aid, atext = answer['aid'], answer['atext']
+        encoded_a = tokenizer.encode(atext, add_special_tokens=True)        
+        padded_a = pad_seq(encoded_a, seq_len=30)
+        input_ids_0.append(padded_q)
+        input_ids_1.append(padded_a)
+        instance_y = 1 if right_answer == aid else 0
+        labels.append(instance_y)
+        
+    attention_masks_0, attention_masks_1 = [], []
+    for sent in input_ids_0:  
+        att_mask = [int(token_id > 0) for token_id in sent]
+        attention_masks_0.append(att_mask)
+    
+    for sent in input_ids_1:  
+        att_mask = [int(token_id > 0) for token_id in sent]
+        attention_masks_1.append(att_mask)
+        
+    return  torch.tensor(input_ids_0),  torch.tensor(attention_masks_0), torch.tensor(input_ids_1),  torch.tensor(attention_masks_1),  torch.tensor(labels)
+
+def evaluator_bert_ir(model, instance, encoder):
+    b_input_ids_0, b_input_mask_0, b_input_ids_1, b_input_mask_1, y = encoder(instance)
+    y_ = model(input_ids_0=b_input_ids_0, 
+              attention_mask_0=b_input_mask_0, 
+              input_ids_1=b_input_ids_1,
+              attention_mask_1=b_input_mask_1,
+              labels=y)
+    pred = torch.max(y_, dim=0)[1]
+    real = torch.max(y, dim=0)[1]
+    acc = (pred == real).float()
+    points = 3 if acc == 1 else -1
+    return acc, points
+
+def encoder_bert(samples, tokenizer):
+    input_ids, labels = [], []
+    for item in samples:
+        sent = item['question'] +' [SEP] ' + item['answer'] 
+        encoded_sent = tokenizer.encode(sent, add_special_tokens = True)
+        padded_sent = pad_seq(encoded_sent, seq_len=30)
+        input_ids.append(padded_sent)
+        labels.append(item['label'])
+        
+    attention_masks = []
+    for sent in input_ids:  
+        att_mask = [int(token_id > 0) for token_id in sent]
+        attention_masks.append(att_mask)
+        
+    return input_ids, attention_masks, labels
+
+def evaluator_bert(model, instance, encoder):
+    b_input_ids_0, b_input_mask_0, y = encoder(instance)
+    y_ = model(b_input_ids_0, 
+              b_input_mask_0,
+              labels=y)
+    y_ = y_.logits
+    pred = torch.max(y_, dim=0)[1]
+    real = torch.max(y, dim=0)[1]
+    acc = (pred == real).float()
+    points = 3 if acc == 1 else -1
+    return acc, points
+
+def encoder_bert_instance(sample, tokenizer=BertTokenizer.from_pretrained('dccuchile/bert-base-spanish-wwm-cased', do_lower_case=False)):
+    qtext, answers = sample['qtext'], sample['answers']
+    right_answer = sample['ra']
+    input_ids, labels = [], []
+    
+    for answer in answers:
+        aid, atext = answer['aid'], answer['atext']
+        sent = qtext + ' [SEP] ' + atext
+        encoded_sent = tokenizer.encode(sent, add_special_tokens=True)
+        padded_sent = pad_seq(encoded_sent, seq_len=30)
+        input_ids.append(padded_sent)
+        instance_y = 1 if right_answer == aid else 0
+        labels.append(instance_y)
+        
+    attention_masks = []
+    for sent in input_ids:  
+        att_mask = [int(token_id > 0) for token_id in sent]
+        attention_masks.append(att_mask)
+        
+    return torch.tensor(input_ids), torch.tensor(attention_masks), torch.tensor(labels)
